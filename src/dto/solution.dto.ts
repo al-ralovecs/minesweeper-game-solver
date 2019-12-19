f92e6a3c63be6fbcc49141da18adb089d60882bb
Aleksandrs <aleksandrs.ralovecs@gmail.com>
2019-12-19 13:45:18 +0200

    (master) code base cleaned.

--- src/dto/solution.dto.ts
+++
@@ -1,3 +0,0 @@
-export default class SolutionDto {
-    private data: boolean[];
-}
--- src/strategy/guess-as-do-or-die.ts
+++
@@ -1,9 +0,0 @@
-import AbstractStrategy from './abstract-strategy';
-
-export default class GuessAsDoOrDie extends AbstractStrategy
-{
-    apply(): void
-    {
-
-    }
-}
