122338875fff9f864fd448a65b3045c8636a2869
Aleksandrs Ralovecs <aleksandrs.ralovecs@gmail.com>
2019-12-24 07:17:22 +0200

    (master) cleaned.

--- src/routine/value.to-bigint.ts
+++
@@ -1,8 +0,0 @@
-export default function toBigInt(value: any): bigint
-{
-    if (typeof value === 'undefined') {
-        return 1n;
-    }
-
-    return BigInt(value);
-}
--- src/routine/value.to-number.ts
+++
@@ -1,8 +0,0 @@
-export default function toNumber(value: any): number
-{
-    if (typeof value === 'undefined' || isNaN(value)) {
-        return 1;
-    }
-
-    return Number(value);
-}
